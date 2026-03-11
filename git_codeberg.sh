2a1cbc0b5c971df924cff88835867e262df2a2ae
pratyush <pratyushp@tutamail.com>
2026-03-11 20:47:31 +0530

    i am dumb as heck

--- git_codeberg.sh
+++ git_codeberg.sh
@@ -48,9 +48,9 @@ echo "enabled ssh-agent"
 ssh-add ~/.ssh/id_ed25519
 cat ~/.ssh/id_ed25519.pub | wl-copy
 echo "the public key has been copied..paste it in the platform of ur choice..personally i'd go to codeberg"
-read -p "enter host url of the platform (leave blank if codeberg.org)" plat
-if [[ $plat != "" ]]; then
-  git -T git@codeberg.org
-else
-  git -T "git@$plat"
-fi
+# read -p "enter host url of the platform (leave blank if codeberg.org)" plat
+# if [[ $plat != "" ]]; then
+#   git -T git@codeberg.org
+# else
+#   git -T "git@$plat"
+# fi
