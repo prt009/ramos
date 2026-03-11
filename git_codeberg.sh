60660db853d87f7f4188cfd6d538a7a7e352418e
pratyush <pratyushp@tutamail.com>
2026-03-11 20:45:13 +0530

    i am so dumb

--- git_codeberg.sh
+++ git_codeberg.sh
@@ -1,4 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/sh
+
+. /etc/os-release
+
+set -eo pipefail
 
 echo "hmm git installation now.. we will set up the mail and name globally"
 if [[ $ID -eq "arch" ]]; then
@@ -8,16 +12,37 @@ else
 fi
 
 echo "[-------git setup-------]"
-read -p "enter the desired email: " umail
-git config --global user.email "$(umail)"
-echo "the email $(umail) has been setup."
-read -p "enter the desired username: " usname
-git config --global user.name "$(usname)"
+while true; do
+  read -rp "enter your desired email: " email
+  if [[ -n "$email" ]]; then
+    break
+  else
+    echo "email cannot be empty."
+  fi
+done
+
+while true; do
+  read -p "enter the desired username: " usname
+  if [[ -n "$usname" ]]; then
+    break
+  else
+    echo "username required"
+  fi
+done
+git config --global user.email "$email"
+git config --global user.name "$usname"
+echo "$email and $usname has been setup"
+
+echo
+echo "git has been setup successfully"
+echo
+
+git config --global --list | grep user
 
 echo ""
 echo "[-------ssh keygens setup-------]"
 echo "now setting up ssh-keygens.."
-ssh-keygen -t ed25519 -C "$(umail)"
+ssh-keygen -t ed25519 -C "$email"
 eval "$(ssh-agent -s)"
 echo "enabled ssh-agent"
 ssh-add ~/.ssh/id_ed25519
@@ -25,7 +50,7 @@ cat ~/.ssh/id_ed25519.pub | wl-copy
 echo "the public key has been copied..paste it in the platform of ur choice..personally i'd go to codeberg"
 read -p "enter host url of the platform (leave blank if codeberg.org)" plat
 if [[ $plat != "" ]]; then
-  git -T git@codeberg.
+  git -T git@codeberg.org
 else
-  git -T git@$plat
+  git -T "git@$plat"
 fi
