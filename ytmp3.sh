43cc23ebcb45c5ed61e37a039baded9ae3cb3140
prt009 <pravanjanp433@gmail.com>
2026-09-05 08:12:04 +0530

    update: yt mp3 download

---
+++ ytmp3.sh
@@ -0,0 +1,1 @@
+yt-dlp --extract-audio --audio-format mp3 --embed-metadata --embed-thumbnail --convert-thumbnails jpg "$1" --ppa 'EmbedThumbnail+ffmpeg_o:-c:v mjpeg -vf crop="'"'"'if(gt(ih,iw),iw,ih)'"':'"'if(gt(iw,ih),ih,iw)'"'"'"' -P $HOME/Music/
